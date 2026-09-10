7c41a34654933f850b79c23a4d3c74e7ccffa48d
Arya Boudaie <arya@aryaboudaie.com>
2026-09-10 10:11:38 -0700

    Fix badge

--- public/sw.js
+++ public/sw.js
@@ -17,7 +17,7 @@ self.addEventListener('push', function (event) {
     const options = {
       body: data.body,
       icon: '/icon-192x192.png',
-      badge: '/icon-192x192.png',
+      badge: '/badge.png',
       vibrate: [100, 50, 100],
       data: {
         dateOfArrival: Date.now(),
